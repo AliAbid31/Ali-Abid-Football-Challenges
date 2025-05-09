07f41de74c446fdda6d7de2741c22ff299f90353
Ali Abid <oa_abid@esi.dz>
2025-05-09 17:22:26 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Les chaînes commençant par '/api' seront redirigées
       '/api': {
-        target: 'http://localhost:3000', // L'URL de votre serveur backend
+        target: 'https://ali-abid-backend', // L'URL de votre serveur backend
         changeOrigin: true, // Nécessaire pour les hôtes virtuels
         // secure: false, // Si votre backend n'utilise pas HTTPS en dev
         // rewrite: (path) => path.replace(/^\/api/, '') // Si votre backend n'a pas le préfixe /api
