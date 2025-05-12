cd3e4e992c8deba3e5c993e6279e98a24bfd3af3
Ali Abid <oa_abid@esi.dz>
2025-05-12 18:01:42 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,10 @@
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { clubs } from '../data/clubs.js'; // Utilisation de clubs.js
+import './Trophies.css'; // Assurez-vous d'avoir un fichier Trophies.css
 
+const TrophiesChallenge = () => {
+
+  );
+};
+
+export default TrophiesChallenge;
